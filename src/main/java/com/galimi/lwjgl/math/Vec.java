dce9cac46d47d8b8aa78626e39d1c2c96ff56bce
griffing52 <emailgriffinnow@gmail.com>
2022-02-05 21:57:12 -0800

    Vectors

---
+++ src/main/java/com/galimi/lwjgl/math/Vec.java
@@ -0,0 +1,10 @@
+package com.galimi.lwjgl.math;
+
+public class Vec {
+    private float x,y;
+
+    public Vec(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/main/java/com/galimi/lwjgl/math/Vec3.java
@@ -0,0 +1,10 @@
+package com.galimi.lwjgl.math;
+
+public class Vec3 extends Vec {
+    private float z;
+    
+    public Vec3(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+}
